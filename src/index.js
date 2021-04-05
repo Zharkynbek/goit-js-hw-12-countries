b0a3d297d5460e039c79405762182d05d3fc8a3f
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-04-05 15:15:32 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -1,53 +1,57 @@
-
 import './styles.css';
-import getCountries from './js/fetch-countries'
-import  { error } from '@pnotify/core';
+import refs from './js/refs.js';
+import getCountries from './js/fetch-countries';
+import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import debounce from 'lodash.debounce';
-import refs from './js/refs'
 
-
-function createMarkup(e) {
-    const query = e.target.value
-    getCountries(query)
-        .then(data => {
-            if (data.length > 1 && data.length <= 10) {
-                const markup = data.map(({ name }) => `<li>${name}</li>`).join('')
-                refs.countriesList.innerHTML = ''
-                refs.countriesList.insertAdjacentHTML('beforeend', markup)
-            }
-            else if (data.length === 1) {
-                console.log(data);
-                const languages = data[0].languages.map(({ name }) => `<li>${name}</li>`).join('')
-                const markup = data.map(({ name, flag, capital, population}) => {
-                    return `<li class="countryList">
+function oneGetCountry(data) {
+  const languages = data[0].languages
+    .map(({ name }) => `<li>${name}</li>`)
+    .join('');
+  const markup = data
+    .map(({ name, flag, population, capital }) => {
+      return `<li>
                     <h1>${name}</h1>
-                    <div class="img"><img src="${flag}" width="280"></div>
+                    <img src="${flag}" alt="${name}" width="320">
                     <p>Capital: ${capital}</p>
                     <p>Population: ${population}</p>
-                    <p>Languages:</p>
                     <ul>${languages}</ul>
-                    </li>`
-                }).join('')
-                refs.countriesList.innerHTML = ''
-                refs.countriesList.insertAdjacentHTML('beforeend', markup)
-                
-            }
-            else if (data.length > 10) {
-                refs.countriesList.innerHTML = ''
-                error({
-                    text: "Too many matches found. Please enter a more specific query!",
-                delay: 2000,
-                })
-            }
-            else {
-                error({
-                text: "Too many matches found!",
-                delay: 2000,
-                })
-            }
-        })
+                    </li>`;
+    })
+    .join('');
+  refs.countriesList.innerHTML = '';
+  refs.countriesList.insertAdjacentHTML('beforeend', markup);
 }
 
-refs.input.addEventListener('input', debounce(createMarkup, 500))
+function createCountries(e) {
+  const query = e.target.value;
+  refs.countriesList.innerHTML = '';
+
+  if (query.trim() !== '') {
+    getCountries(query.trim()).then(data => {
+      console.log(data);
+      if (data.length > 1 && data.length <= 10) {
+        const markup = data.map(({ name }) => `<li>${name}</li>`).join('');
+        refs.countriesList.innerHTML = '';
+        refs.countriesList.insertAdjacentHTML('beforeend', markup);
+      } else if (data.length === 1) {
+        oneGetCountry(data);
+      } else if (data.length > 10) {
+        error({
+          text: 'too many matches found',
+          delay: 2000,
+        });
+      } else {
+        error({
+          text: 'not found',
+          delay: 2000,
+        });
+        refs.countriesList.innerHTML = '';
+      }
+    });
+  }
+}
+
+refs.input.addEventListener('input', debounce(createCountries, 500));
