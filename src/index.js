ceb97230c136d09898d4716b156d88a1a5601312
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-04-05 16:36:22 +0200

    add axios

--- src/index.js
+++ src/index.js
@@ -55,3 +55,63 @@ function createCountries(e) {
 }
 
 refs.input.addEventListener('input', debounce(createCountries, 500));
+
+// ======================== with axios ================================
+
+// import './styles.css';
+// import refs from './js/refs.js';
+// import getCountries from './js/fetch-countries';
+// import { error } from '@pnotify/core';
+// import '@pnotify/core/dist/PNotify.css';
+// import '@pnotify/core/dist/BrightTheme.css';
+// import debounce from 'lodash.debounce';
+
+// function oneGetCountry(data) {
+//   const languages = data[0].languages
+//     .map(({ name }) => `<li>${name}</li>`)
+//     .join('');
+//   const markup = data
+//     .map(({ name, flag, population, capital }) => {
+//       return `<li>
+//                     <h1>${name}</h1>
+//                     <img src="${flag}" alt="${name}" width="320">
+//                     <p>Capital: ${capital}</p>
+//                     <p>Population: ${population}</p>
+//                     <ul>${languages}</ul>
+//                     </li>`;
+//     })
+//     .join('');
+//   refs.countriesList.innerHTML = '';
+//   refs.countriesList.insertAdjacentHTML('beforeend', markup);
+// }
+
+// function createCountries(e) {
+//   const query = e.target.value;
+//   refs.countriesList.innerHTML = '';
+
+//   if (query.trim() !== '') {
+//     getCountries(query.trim()).then(data => {
+//       console.log(data);
+//       if (data.length > 1 && data.length <= 10) {
+//         const markup = data.map(({ name }) => `<li>${name}</li>`).join('');
+//         refs.countriesList.innerHTML = '';
+//         refs.countriesList.insertAdjacentHTML('beforeend', markup);
+//       } else if (data.length === 1) {
+//         oneGetCountry(data);
+//       } else if (data.length > 10) {
+//         error({
+//           text: 'too many matches found',
+//           delay: 2000,
+//         });
+//       } else {
+//         error({
+//           text: 'not found',
+//           delay: 2000,
+//         });
+//         refs.countriesList.innerHTML = '';
+//       }
+//     });
+//   }
+// }
+
+// refs.input.addEventListener('input', debounce(createCountries, 500));
--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -1,5 +1,15 @@
 export default async function getCountries(countryName) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
-        .then(resp => resp.json())
-        .catch(() => console.log('error'))
+  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+    .then(resp => resp.json())
+    .catch(() => console.log('error'));
 }
+
+// ======================== with axios ================================
+
+// import axios from 'axios';
+// export default async function getCountries(countryName) {
+//   return axios
+//     .get(`https://restcountries.eu/rest/v2/name/${countryName}`)
+//     .then(resp => resp.data)
+//     .catch(error => console.log(error));
+// }
